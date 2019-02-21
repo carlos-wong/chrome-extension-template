a8b49715724c8f434a21d1409b094677b921d3b5
carlos <huaixian.huang@gmail.com>
2019-02-21 20:29:09 +0800

    feat: comment #pass work now

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,35 @@
 window.addEventListener ("load", myMain, false);
+var gitlab = require('./gitlab');
+
+function InitPassBtn(){
+  //
+  let commentDiv = document.querySelector('#notes > div > ul > li > div > div.timeline-content.timeline-content-form > form > div.note-form-actions');
+  let closeissueBtn = document.querySelector('#notes > div > ul > li > div > div.timeline-content.timeline-content-form > form > div.note-form-actions > button');
+  if(commentDiv && closeissueBtn){
+    let commentBtn = document.createElement("Button");       // Create a <li> node
+    commentBtn.className = closeissueBtn.className;
+    var textnode = document.createTextNode("Pass Softdev Code Review");  // Create a text node
+    commentBtn.appendChild(textnode);
+    commentBtn.addEventListener('click', function() {
+      HandlePassClick();
+    });
+    commentDiv.appendChild(commentBtn);
+  }
+}
+
+function HandlePassClick(){
+  let curURL = document.URL;
+  let urlInfo = gitlab.GitlabParseURLInfo(curURL);
+  // gitlab.GitlabCommentMr(urlInfo.project,urlInfo.mr,"#pass",(data)=>{});
+}
 
 function myMain(){
-  console.log('Hello world for gitlab code review');
+  let curURL = document.URL;
+  let urlInfo = gitlab.GitlabParseURLInfo(curURL);
+  gitlab.QueryProjectMr(urlInfo.project,urlInfo.mr,(data)=>{
+    let assigneeUsername = data.assignee && data.assignee.username;
+    if(assigneeUsername === "carlos_softdev_merge_bot"){
+      InitPassBtn();
+    }
+  });
 }
---
+++ gitlab.js
@@ -0,0 +1,74 @@
+var axios = require('axios');
+var config = require('./config');
+var lodash = require('lodash');
+var gitlab_axios_instance;
+
+chrome.storage.sync.get({
+    lejuhubtoken: '',
+  }, function(items) {
+    config.token = items.lejuhubtoken;
+    gitlab_axios_instance = axios.create({
+      baseURL: config.api_url,
+      timeout: 10000,
+      headers: { "PRIVATE-TOKEN": config.token}
+    });
+  });
+
+function QueryProjectMr(project,iid,callback){
+  gitlab_axios_instance
+    .get(
+      "/projects/" +
+        encodeURIComponent(project) +
+        "/merge_requests/"+iid
+    )
+    .then(data => {
+      callback(data.data);
+    })
+    .catch((err)=>{
+      callback(err,null);
+    });
+}
+
+function GitlabCommentMr(project_id,iid,comment,callback){
+  gitlab_axios_instance
+    .post("/projects/"+encodeURIComponent(project_id)+"/merge_requests/"+iid+"/notes",{
+      body:comment
+    })
+    .then(()=>{
+      callback();
+    })
+    .catch((error)=>{
+      callback(error);
+    });
+}
+
+function GitlabCommentissue(project_id,iid,comment,callback){
+  gitlab_axios_instance
+    .post("/projects/"+encodeURIComponent(project_id)+"/issues/"+iid+"/notes",{
+      body:comment
+    })
+    .then(()=>{
+      callback();
+    })
+    .catch((error)=>{
+      callback(error);
+    });
+}
+
+function GitlabParseURLInfo(url){
+  let projectInfo = {};
+  [projectInfo.groupname,projectInfo.projectname,projectInfo.type,projectInfo.mr] =  lodash.split(lodash.split(url,"http://www.lejuhub.com/")[1],'/');
+  projectInfo.project = projectInfo.groupname + '/' + projectInfo.projectname;
+  projectInfo.mr = parseInt(projectInfo.mr);
+  return projectInfo;
+}
+
+
+let api={};
+
+api.QueryProjectMr = QueryProjectMr;
+api.GitlabCommentMr= GitlabCommentMr;
+api.GitlabCommentissue = GitlabCommentissue;
+api.GitlabParseURLInfo = GitlabParseURLInfo;
+
+module.exports = api;
